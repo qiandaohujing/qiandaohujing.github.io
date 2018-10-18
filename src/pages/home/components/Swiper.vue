bae710d27d548761a517c6abe7a9098b2dd54a33
qiandaohujing <43477065+qiandaohujing@users.noreply.github.com>
2018-10-18 23:03:35 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+        <!-- slides -->
+        <swiper-slide v-for="item of swiperList" :key="item.id">
+            <img class="swiper-img" :src="item.imgUrl" />
+        </swiper-slide>
+        <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1810/fe/fd9c295c0e1a7602.jpg_750x200_11c8343f.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1712/51/36632b2cb850e902.jpg_750x200_fa4120db.jpg'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/69/8697f10f17f35a02.jpg_750x200_c98193a9.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background #ffffff
+    .wrapper
+        overflow: hidden
+        width 100%
+        height 0
+        padding-bottom 26.67%
+        .swiper-img
+            width 100%
+</style>
