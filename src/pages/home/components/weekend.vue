78e5bdc7887efa894a4d99897a0500ac8391bf33
qiandaohujing <43477065+qiandaohujing@users.noreply.github.com>
2018-10-20 02:10:58 +0800

    index finish

--- src/pages/home/components/weekend.vue
+++ src/pages/home/components/weekend.vue
@@ -2,7 +2,7 @@
     <div>
         <div  class="title">周末去哪</div>
         <ul>
-            <li class="item border-bottom" v-for="item in recommendList" :key="item.id">
+            <li class="item border-bottom" v-for="item in list" :key="item.id">
                 <div class="item-img-wrapper">
                     <img class="item-img" :src="item.imgUrl" />
                 </div>
@@ -18,25 +18,8 @@
 <script>
 export default {
   name: 'HomeWeekend',
-  data () {
-    return {
-      recommendList: [{
-        id: '0001',
-        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/4e/3e87ee62e59cdb.jpg_r_640x214_fe6cc902.jpg',
-        title: '杭州云曼温泉',
-        desc: '杭州云曼温泉杭州云曼温泉杭州云曼温泉'
-      }, {
-        id: '0002',
-        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/7d45cc38259e0.jpg_r_640x214_156f4299.jpg',
-        title: '杭州野生动物世界',
-        desc: '杭州野生动物世界杭州野生动物世界杭州野生动物世界'
-      }, {
-        id: '0003',
-        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/bc/83feb2e003d0dd.jpg_r_640x214_0b7269b8.jpg',
-        title: '杭州海底世界',
-        desc: '杭州海底世界杭州海底世界杭州海底世界'
-      }]
-    }
+  props: {
+    list: Array
   }
 }
 </script>
@@ -44,14 +27,13 @@ export default {
 <style lang="stylus" scoped>
   @import '~styles/mixins.styl'
   .title
-    margin-top: .2rem
     line-height .8rem
     background #eee
     text-indent .2rem
   .item-img-wrapper
     overflow hidden
     height 0
-    padding-bottom 33.9%
+    padding-bottom 37.09%
     .item-img
         width 100%
   .item-info
