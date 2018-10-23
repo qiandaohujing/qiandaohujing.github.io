bcb9951d3a66a8e46b0a79739103331177358a2f
qiandaohujing <43477065+qiandaohujing@users.noreply.github.com>
2018-10-23 17:16:11 +0800

    search finish

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -17,11 +17,11 @@
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="area" v-for="(item,key) of cities" :key="key">
-            <div class="title border-topbottom">{{key}}</div>
-            <div class="item-list">
-                <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">{{innerItem.name}}</div>
+            <div class="area" v-for="(item,key) of cities" :key="key">
+                <div class="title border-topbottom">{{key}}</div>
+                <div class="item-list">
+                    <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id" :ref="key">{{innerItem.name}}</div>
+                </div>
             </div>
         </div>
     </div>
@@ -33,10 +33,19 @@ export default {
   name: 'CityList',
   props: {
     hot: Array,
-    cities: Object
+    cities: Object,
+    letter: String
   },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 }
 </script>
@@ -52,6 +61,7 @@ export default {
     &:before
       border-color #ccc
   .list
+    overflow hidden
     position absolute
     top 1.58rem
     left 0
